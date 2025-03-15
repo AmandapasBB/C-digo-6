862ac6010979e21745f06e6e0dd36084b8f65e88
Amanda Alves <165097403+AmandapasBB@users.noreply.github.com>
2025-03-15 13:35:44 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("./db");
+const cors = require("cors");
 const filmesRoutes = require("./routes/filmesRoutes");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api", filmesRoutes);
 
